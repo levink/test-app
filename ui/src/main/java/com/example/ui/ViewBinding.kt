7b9371e3b7d59a0c3c0a4931c4abefc59a7d2b4d
levink <levinkostya@yandex.ru>
2021-07-07 17:00:27 +0300

    view binding fix

--- ui/src/main/java/com/example/ui/ViewBinding.kt
+++ ui/src/main/java/com/example/ui/ViewBinding.kt
@@ -1,10 +1,29 @@
 package com.example.ui
 
+import android.view.View
+import androidx.annotation.IdRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.LifecycleOwner
 
-open class ViewBinding (lifecycleOwner: LifecycleOwner){
+open class ViewBinding (lifecycleOwner: LifecycleOwner) {
     private val viewBindingObserver = ViewBindingObserver(lifecycleOwner)
     protected fun <T> ifReady(block: () -> T) : T {
         return viewBindingObserver.ifReady(block)
+    }
+}
+
+open class FragmentViewBinding(fragment: Fragment) : ViewBinding(fragment) {
+    private val view: View = fragment.requireView()
+    protected fun<T> find(@IdRes id : Int): T {
+        return view.findViewById(id)
+    }
+}
+
+open class ActivityViewBinding(
+    private val activity: AppCompatActivity
+) : ViewBinding(activity) {
+    protected fun<T> find(@IdRes id : Int): T {
+        return activity.findViewById(id)
     }
 }
